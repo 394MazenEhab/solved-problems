29baece2efa64e2d51234ff14d649e078bf17c86
Mazen Ehab <mazensamaha17@gmail.com>
2026-02-24 06:00:54 +0200

    Add Functions directory and it's solutions

---
+++ Functions/print_name.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void PrintName(string Name) {
+    cout << "\nYour name is: "<<Name<< endl;
+}
+
+int main()
+{
+    PrintName("Mazen");
+}
